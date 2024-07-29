bbddff2c77a0e27c4986c33448f36afc69d6d0e2
krk <72791356+scrooge001@users.noreply.github.com>
2024-07-29 23:15:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ PICS = (environ.get('PICS' ,'https://telegra.ph/file/8d522450cbe7351268cbf.jpg')
 BOT_START_TIME = time()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7044258634').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('', '').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
